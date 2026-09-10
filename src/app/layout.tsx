19423b3a660a0f240586c18a68e2116843b239bd
Lesylvvv <293332871+Lesylvvv@users.noreply.github.com>
2026-09-10 09:40:02 +0200

    feat: add global Metricool tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
     <html lang="fr" className={`${manrope.variable} h-full`}>
       <body>
         {children}
+        <Script id="metricool-tracker" strategy="afterInteractive">
+          {`function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"15dd4ecad95ae1b9bfc6ba5e26ddb301"})});`}
+        </Script>
         <Script
           id="hs-script-loader"
           type="text/javascript"
